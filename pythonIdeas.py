ab2a848955c26777f3ccd7d007b30d77e6e90d6f
Lucas Lassinger <lblassinger@gmail.com>
2020-04-24 12:53:38 -0400

    Initial Commit

---
+++ pythonIdeas.py
@@ -0,0 +1,13 @@
+#Python Ideas
+print("AITA")
+print("Binary Writer")
+print('Substitute teacher Checker')
+print('language converter')
+print('Scrabble game')
+print('Statics Solver')
+print('Statistics functions')
+print('HillcrestJamestown App')
+print('Path finding visualizer algorythym')
+print('Space wars game with GUI to beat it')
+print('Rubiks cube solver')
+print('Automated code uploader (Github/ IPad')
